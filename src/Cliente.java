94e0c973400cd73a38d1819a186bbd84d59a3932
stinfire <61717657+stinfire@users.noreply.github.com>
2020-03-02 22:06:44 -0300

    desenvolvimento uib

---
+++ src/Cliente.java
@@ -0,0 +1,24 @@
+
+public class Cliente {
+
+	public int numero;
+	public double saldo;
+	public String nome;
+	public String cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome=nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
---
+++ src/conta.java
@@ -0,0 +1,21 @@
+
+public class conta {
+
+	public int numero;
+	public double saldo;
+	public String dono;
+	
+	
+	public String getDono() {
+		return dono;
+	}
+	public void setDono(String dono) {
+		this.dono = dono;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
